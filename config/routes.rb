10b5c40556c671e8f6ce9ba9ef1f358ec5c97841
alban porcheron <alban.porcheron@gmail.com>
2018-03-02 18:11:40 +0100

    Fixed a small bug on routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
 
-  get "landing" to: 'pages#landing_liquidity'
-  get "home" to: 'pages#landing_transparency'
-  get "about" to: 'pages#about_us'
+  get "landing" => 'pages#landing_liquidity'
+  get "home" => 'pages#landing_transparency'
+  get "about" => 'pages#about_us'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
