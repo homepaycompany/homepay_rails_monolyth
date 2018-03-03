196a4a1a65ef18d84bf1f4ed2019c27fb6ee9b73
arthur reboul <arthur.reboul@gmail.com>
2018-03-03 18:54:16 +0100

    bootstrap grid on property_form show

--- app/javascript/packs/map.js
+++ app/javascript/packs/map.js
@@ -5,6 +5,6 @@ if (mapElement) { // don't try to build a map if there's no div#map to inject in
   const map = new GMaps({ el: '#map', lat: 0, lng: 0 });
   const markers = [JSON.parse(mapElement.dataset.markers)];
   map.addMarkers(markers);
-  map.setZoom(14);
+  map.setZoom(16);
   map.setCenter(markers[0].lat, markers[0].lng);
 }
