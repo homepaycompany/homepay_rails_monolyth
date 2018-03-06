0cb24362ec0feab2ed74b308fecc769081f9b682
alban porcheron <alban.porcheron@gmail.com>
2018-03-06 19:16:56 +0100

    Revert "Merge branch 'master' into 'landing-page-liquidity'"
    This reverts commit 4a37beab58295ef96cf5fc48038d2f97782e70b5, reversing
    changes made to d56864857efbd2c916307cb118228cb75503550f.

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,6 +1,13 @@
 import "bootstrap";
-import { autocomplete } from '../components/autocomplete'
 
+// Javascript for autocomplete
+import { autocomplete } from '../components/autocomplete'
 if (document.getElementById('real_estate_property_address')) {
   autocomplete();
+}
+
+// Javascript for landing page
+import { waypoint } from '../shared/landing_page.js'
+if (document.getElementById('js-value-prop-sale')) {
+  waypoint();
 }
--- config/routes.rb
+++ config/routes.rb
@@ -5,13 +5,8 @@ Rails.application.routes.draw do
   get "landing" => 'pages#landing_liquidity'
   get "home" => 'pages#landing_transparency'
   get "about" => 'pages#about_us'
+  get "how-to" => 'pages#how_to'
 
-  resources :property_forms, only: [:new, :show] do
-    put "description_1" => 'property_forms#description_1'
-    put "description_2" => 'property_forms#description_2'
-    put "description_3" => 'property_forms#description_3'
-    put "description_4" => 'property_forms#description_4'
-    put "personnal_informations" => 'property_forms#personnal_informations'
-  end
+  resources :property_forms
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
