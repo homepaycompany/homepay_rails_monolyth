296415a193c5dd2ced962244e2868e300b482ea5
arthur reboul <arthur.reboul@gmail.com>
2018-03-07 15:49:50 +0100

    modified config.asset.precompile to true for heroku deploy of assets

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,6 +2,6 @@ class UserMailer < ApplicationMailer
   def confirm_form(user_id, property)
     @user = User.find(user_id)  # Instance variable => available in view
     @property = property
-    mail(to: @user.email, subject: 'Welcome to Le Wagon')
+    mail(to: @user.email, subject: 'Bienvenue sur Homepay')
   end
 end
