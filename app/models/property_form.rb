17356e20f016c3087d57477a6718707c738a4d0a
arthur reboul <arthur.reboul@gmail.com>
2018-03-18 17:52:22 +0100

    migration

--- app/models/property_form.rb
+++ app/models/property_form.rb
@@ -1,6 +1,6 @@
 class PropertyForm < ApplicationRecord
   geocoded_by :address
-  after_validation :geocode, if: :will_save_change_to_address?
+  # after_validation :geocode, if: :will_save_change_to_address?
 
   has_many :property_images
   has_one :real_estate_property
