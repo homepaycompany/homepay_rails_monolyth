e77271008bd05bcdbcc16646d33701acf52cf36c
alban porcheron <alban.porcheron@gmail.com>
2018-03-20 14:23:51 +0100

    Added JS to show/hide questions and answers on landing page

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,13 +1,13 @@
 import "bootstrap";
-import { autocomplete } from '../components/autocomplete'
-import { goBack } from '../components/goBack'
-import { formSectionValidation } from '../components/formSectionValidation'
-import { setChoiceForm } from '../components/formRevealElements'
-import { addBooleanToForm } from '../components/formCheckBoolean'
-import { addStateToForm } from '../components/formCheckStateBox'
-import { waypoint } from '../shared/landing_page.js'
-import { validateAddress } from '../components/addressValidation'
-import { launchDropzone } from '../components/dropzone'
+import { autocomplete } from '../shared/autocomplete'
+import { goBack } from '../shared/goBack'
+import { formSectionValidation } from '../shared/formSectionValidation'
+import { setChoiceForm } from '../shared/formRevealElements'
+import { addBooleanToForm } from '../shared/formCheckBoolean'
+import { addStateToForm } from '../shared/formCheckStateBox'
+import { validateAddress } from '../shared/addressValidation'
+import { launchDropzone } from '../shared/dropzone'
+import { toggleFaq } from '../shared/faq'
 
 if (document.getElementById('js-go-back')) {
   goBack();
@@ -29,10 +29,11 @@ if (document.getElementById('js-form-state-box')) {
   addStateToForm();
 }
 
-// Javascript for landing page
-if (document.getElementById('js-value-prop-sale')) {
-  waypoint();
+// JS for FAQ toggle
+if (document.getElementById('js-faq-question')) {
+  toggleFaq();
 }
+
 
 // Do not launch JS for address autocomplete and address validation on Review apps, as Google
 // browser API restrictions don't allow them to use the API
