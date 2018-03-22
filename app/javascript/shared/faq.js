0795725b00bc4e70e701967d7b5cdb0b83109a76
alban porcheron <alban.porcheron@gmail.com>
2018-03-22 15:19:37 +0100

    Added javascript for FAQ page

--- app/javascript/shared/faq.js
+++ app/javascript/shared/faq.js
@@ -1,4 +1,5 @@
-function toggleClass() {
+// JS to open the answer related to a specific question in FAQ
+function showQuestion() {
   const childrens = event.currentTarget.childNodes;
   const answerId = event.currentTarget.dataset.question;
   for (var i=0; i<childrens.length; i++) {
@@ -10,10 +11,28 @@ function toggleClass() {
   const answer = document.querySelector(`#js-faq-answer[data-answer="${answerId}"]`);
   answer.classList.toggle("hidden");
 }
-
 function toggleFaq() {
   const faqs = document.querySelectorAll("#js-faq-question");
-  faqs.forEach(faq => faq.addEventListener('click', toggleClass));
+  faqs.forEach(faq => faq.addEventListener('click', showQuestion));
 }
 
-export { toggleFaq };
+
+// JS to open the answer related to a specific topic in FAQ
+function showFaqSection() {
+  const selectedFaqSection = event.currentTarget;
+  const activeFaqSection = document.querySelector(".faq-values .active");
+  const topic = event.currentTarget.dataset.faq;
+  const selectedWrapper = document.querySelector(`.faq-wrapper[data-faq="${topic}"]`);
+  const activeWrapper = document.querySelector(".faq-wrapper-active");
+
+  selectedFaqSection.classList.toggle("active");
+  activeFaqSection.classList.toggle("active");
+  selectedWrapper.classList.toggle("faq-wrapper-active");
+  activeWrapper.classList.toggle("faq-wrapper-active");
+}
+function faqSection() {
+  const faqSections = document.querySelectorAll(".faq-values h3");
+  faqSections.forEach(faqSection => faqSection.addEventListener('click', showFaqSection));
+}
+
+export { toggleFaq, faqSection };
